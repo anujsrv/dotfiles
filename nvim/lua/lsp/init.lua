8a022920febf7951c9e5c1fb83088a4e3fd29f1b
Anuj Srivastava <asrivas1@asrivas1-mn3.linkedin.biz>
2022-08-29 21:46:39 +0530

    rewrite LspInstall command in lua

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -4,7 +4,7 @@ local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>eq', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
--- nvim/lua/lsp/installer.lua
+++ nvim/lua/lsp/installer.lua
@@ -1,7 +1,18 @@
-function LspInstall(language)
+vim.api.nvim_create_user_command("LspInstall", function() 
+    local language = vim.fn.input('language (supported: go, java, python): ')
     local cmd = '~/.vim/installer/lsp-installers.sh ' .. language
-    local out = vim.fn.system(cmd)
-    vim.cmd('echom', out)
-end
-vim.api.nvim_command("command! -nargs=* LspInstall call luaeval('LspInstall(_A)', expand('<q-args>'))")
-
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.cmd"botright split"
+    vim.cmd("buffer "..bufnr)
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {"installation stdout/stderr: (please wait for installation to complete)"} )
+    local echo_out = function(_, data)
+        if data then
+            vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+        end
+    end
+    vim.fn.jobstart(cmd, {
+        stdout_buffered = true,
+        on_stdout = echo_out,
+        on_stderr = echo_out
+    })
+end, {})
