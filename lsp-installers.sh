4547aa3af13890dd052f78d795c0973615ef048e
Anuj Srivastava <asrivastava1@linkedin.com>
2025-08-20 06:36:18 +0000

    update lsp installers script

--- lsp-installers.sh
+++ lsp-installers.sh
@@ -11,21 +11,28 @@ if [ "$1" = "python" ]; then
     npm install pyright
     ln -s "./node_modules/.bin/pyright-langserver" .
 elif [ "$1" = "go" ]; then
+    ver=`go version | { read _ _ v _; echo ${v#go}; }`
     echo "installing gopls"
-    if [ "$2" != "" ]; then
-        GOPATH=$(pwd) GOBIN=$(pwd) GOROOT=$2 GO111MODULE=on $2/bin/go get -v golang.org/x/tools/gopls@latest
-        GOPATH=$(pwd) GOROOT=$2 GO111MODULE=on $2/bin/go clean -modcache
+    echo "detected golang version: " $ver
+    if [[ "$ver" > "1.17.1" ]]; then
+        GOPATH=$(pwd) GOBIN=$(pwd) go install -v golang.org/x/tools/gopls@latest
     else
-        GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
-        GOPATH=$(pwd) GO111MODULE=on go clean -modcache
+        if [ "$2" != "" ]; then
+            GOPATH=$(pwd) GOBIN=$(pwd) GOROOT=$2 GO111MODULE=on $2/bin/go get -v golang.org/x/tools/gopls@latest
+            GOPATH=$(pwd) GOROOT=$2 GO111MODULE=on $2/bin/go clean -modcache
+        else
+            GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
+            GOPATH=$(pwd) GO111MODULE=on go clean -modcache
+        fi
     fi
     rm -rf src pkg 2> /dev/null
 elif [ "$1" = "java" ]; then
     echo "installing eclipse-jdtls"
-    wget "https://ftp.fau.de/eclipse/jdtls/snapshots/jdt-language-server-latest.tar.gz"
+    # download the version of jdtls from eclipse in given location
+    wget -q --show-progress --progress=bar:force:noscroll "https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.31.0/jdt-language-server-1.31.0-202401111522.tar.gz" -O jdt-language-server.tar.gz
     mkdir eclipse-jdtls
-    tar -xvf jdt-language-server-latest.tar.gz -C eclipse-jdtls
-    rm jdt-language-server-latest.tar.gz
+    tar -xvf jdt-language-server.tar.gz -C eclipse-jdtls
+    rm jdt-language-server.tar.gz
 else
     echo "unknown option: $1"
 fi
